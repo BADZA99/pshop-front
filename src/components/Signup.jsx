8d0ac8940d64a43e5882acbce497234a26071ec4
papabn <pndiaye999@gmail.com>
2024-08-24 19:40:22 +0000

    signup page

---
+++ src/components/Signup.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const title = "Register";
+const socialTitle = "login with social media";
+const btnText = "Register now";
+export default function Signup() {
+  return (
+    <div className="login-section padding-tb section-bg">
+      <div className="container">
+        <div className="account-wrapper">
+          <h5 className="title">{title}</h5>
+          <form className="account-form">
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="name"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+            {/* password */}
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="confirm password"
+                className="form-control"
+                placeholder="confirm password"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="lab-btn lab-btn">
+                <span>{btnText}</span>
+              </button>
+            </div>
+          </form>
+
+          {/* accoiunt button */}
+          <div className="account-bottom">
+            <span className="d-flex cate pt-10">
+               have a account ?<Link to="/login">register now</Link>
+            </span>
+
+            <span className="or">
+              <span>or</span>
+            </span>
+            {/* social icons */}
+            <h5 className="subtitle">{socialTitle}</h5>
+            <ul className="lab-ul social-icons justify-content-center">
+              <li>
+                <button className="github">
+                  <i className="icofont-github"></i>
+                </button>
+              </li>
+              <li>
+                <a href="" className="facebook">
+                  <i className="icofont-facebook"></i>
+                </a>
+              </li>
+
+              <li>
+                <a href="" className="twitter">
+                  <i className="icofont-twitter"></i>
+                </a>
+              </li>
+
+              <li>
+                <a href="" className="linkedin">
+                  <i className="icofont-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -24,44 +24,48 @@ import SingleProduct from './shop/SingleProduct.jsx';
 import CartPage from './shop/CartPage.jsx';
 import About from './About/About.jsx';
 import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
-const router = createBrowserRouter( [
-    {
-      path: "/",
-      element: <App />,
-      children:[
-        {
-          path: "/",
-          element: <Home/>,
-        },
-        {
-          path: "/blog",
-          element: <Blog/>,
-        },
-        {
-          path: "/shop",
-          element: <Shop/>,
-        },
-        {
-            path: "/shop/:id",
-          element: <SingleProduct/>,
-        },
-        {
-          path:'cart-page',
-          element: <CartPage/>,
-        },
-        {
-          path:'/about',
-          element: <About/>,
-        },
-        {
-          path: "/login",
-          element: <Login/>,
-        }
-      ]
-    },
-  ],
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/shop/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart-page",
+        element: <CartPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
